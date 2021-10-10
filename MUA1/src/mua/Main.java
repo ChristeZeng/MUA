2a8a90e38c156c995bd9b66dba192fd697810597
Christe Zeng <christezeng@gmail.com>
2021-10-10 12:23:56 +0800

    Begin MUA1

---
+++ MUA1/src/mua/Main.java
@@ -0,0 +1,9 @@
+package src.mua;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        Interpreter MuaInterpreter = new Interpreter();
+        MuaInterpreter.Run();
+    }
+}
