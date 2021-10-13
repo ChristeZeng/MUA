5f75e7e07254525e2b87e683098aa955076ac098
Christe Zeng <christezeng@gmail.com>
2021-10-13 19:57:49 +0800

    MUA1初步架构形成

--- MUA1/src/mua/Main.java
+++ MUA1/src/mua/Main.java
@@ -3,7 +3,16 @@ package src.mua;
 public class Main {
 
     public static void main(String[] args)  {
-        Interpreter MuaInterpreter = new Interpreter();
-        MuaInterpreter.Run();
+        // Interpreter MuaInterpreter = new Interpreter();
+        // MuaInterpreter.Run();
+        // Word t = new Word();
+        // Word a = new Word();
+
+        // t.assign("123");
+        // a.assign("2");
+        // Word c = t.mod(a);
+        // System.out.println(c.getNumber());
+        Interpreter i = new Interpreter();
+        i.getInput();
     }
 }
--- MUA1/src/mua/Word.java
+++ MUA1/src/mua/Word.java
@@ -1,2 +1,52 @@
-package src.mua
+package src.mua;
 
+public class Word {
+    private String value;
+    private int Type;
+
+    //构造函数暂空
+    public Word() {
+        
+    }
+    public void assign(String value, int Type) {
+        this.value = value;
+        this.Type = Type;
+    }
+    public String getString() {
+        return value;
+    }
+    public float getNumber() {
+        return Float.parseFloat(value);
+    }
+    public Boolean getBoolean() {
+        return Boolean.parseBoolean(value);
+    }
+    public void print() {
+        System.out.println(value);
+    }
+
+    /*运算符*/
+    public Word add(Word oparetor) {
+        Word res = new Word();
+        res.value = Float.toString(oparetor.getNumber() + getNumber());
+        return res;
+    }
+    
+    public Word mul(Word oparetor) {
+        Word res = new Word();
+        res.value = Float.toString(oparetor.getNumber() * getNumber());
+        return res;
+    }
+
+    public Word div(Word oparetor) {
+        Word res = new Word();
+        res.value = Float.toString(getNumber() / oparetor.getNumber());
+        return res;
+    }
+
+    public Word mod(Word oparetor) {
+        Word res = new Word();
+        res.value = Float.toString(getNumber() % oparetor.getNumber());
+        return res;
+    }
+}
